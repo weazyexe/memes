c2daa379ab832e98f9ba5f2c23f17f014b9c5334
weazyexe <weazy.exe@gmail.com>
2020-04-11 12:14:36 +0300

    2-auth | rename description field

--- app/src/main/java/exe/weazy/memes/entity/UserInfo.kt
+++ app/src/main/java/exe/weazy/memes/entity/UserInfo.kt
@@ -1,6 +1,11 @@
 package exe.weazy.memes.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class UserInfo(
     val id: Int, val username: String, val firstName: String,
-    val lastName: String, val userDescription: String
+    val lastName: String,
+
+    @SerializedName("userDescription")
+    val description: String
 )
