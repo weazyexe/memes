4e9ca09f3746399912d9388e7425ba454aa400ba
weazyexe <weazy.exe@gmail.com>
2020-04-12 17:10:16 +0300

    0-fixes | separate extensions

--- app/src/main/java/exe/weazy/memes/util/values.kt
+++
@@ -1,9 +0,0 @@
-package exe.weazy.memes.util
-
-const val PREFERENCES_FILENAME = "MEMES_PREFS"
-
-const val DEFAULT_ID = 0
-const val DEFAULT_USERNAME = "weazyexe"
-const val DEFAULT_FIRST_NAME = "Timur"
-const val DEFAULT_LAST_NAME = "Zadvornov"
-const val DEFAULT_DESCRIPTION = "oh wow, description"
