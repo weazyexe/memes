e901452c56e2b6bdae7ae49b7ea9ba1f407b9601
weazyexe <weazy.exe@gmail.com>
2020-04-14 20:22:07 +0300

    3-memes | create main screen with bottom nav & window insets

---
+++ app/src/main/java/exe/weazy/memes/ui/main/MemesFragment.kt
@@ -0,0 +1,15 @@
+package exe.weazy.memes.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import exe.weazy.memes.R
+
+class MemesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_memes, container, false)
+    }
+}
