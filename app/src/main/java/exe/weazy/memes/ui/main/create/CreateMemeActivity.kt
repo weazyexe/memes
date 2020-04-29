240971c2068a754b1f7d26248de106debac0a3b9
weazyexe <weazy.exe@gmail.com>
2020-04-29 13:54:16 +0300

    4-create | make create view layout

--- app/src/main/java/exe/weazy/memes/ui/main/create/CreateMemeActivity.kt
+++ app/src/main/java/exe/weazy/memes/ui/main/create/CreateMemeActivity.kt
@@ -3,11 +3,20 @@ package exe.weazy.memes.ui.main.create
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import exe.weazy.memes.R
+import kotlinx.android.synthetic.main.activity_create_meme.*
 
 class CreateMemeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_meme)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        closeButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
